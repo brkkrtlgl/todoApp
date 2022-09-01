42e4cb2cf657f36e7fda0a76be7ea4faea73b2c4
brkkrtlgl <burakkartaloglu@gmail.com>
2022-09-01 21:08:53 +0300

    project completed

--- src/App.js
+++ src/App.js
@@ -1,32 +1,110 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   const [todoText, setTodoText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [willUpdateTodo, setWillUpdateTodo] = useState("");
+
+  useEffect(() => {
+    const todosFromLocalStorage = localStorage.getItem("todos");
+    console.log(todosFromLocalStorage);
+    if (todosFromLocalStorage === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      setTodos(JSON.parse(todosFromLocalStorage));
+    }
+  }, []);
+
+  const deleteTodo = (id) => {
+    console.log(id);
+    const filteredTodos = todos.filter((item) => item.id !== id);
+    setTodos(filteredTodos);
+    localStorage.setItem("todos", JSON.stringify(filteredTodos));
+  };
+
+  const changeIsDone = (id) => {
+    const searchedTodo = todos.find((item) => item.id === id);
+    const updatedTodo = {
+      ...searchedTodo,
+      isDone: !searchedTodo.isDone,
+    };
+    const filteredTodos = todos.filter((item) => item.id !== id);
+    setTodos([updatedTodo, ...filteredTodos]);
+    localStorage.setItem(
+      "todos",
+      JSON.stringify([updatedTodo, ...filteredTodos])
+    );
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     if (todoText === "") {
       alert("Todo text can't be empty!");
       return;
     }
-    console.log(todoText);
+    const hasTodos = todos.find((item) => item.text === todoText);
+    console.log(hasTodos);
+    if (hasTodos !== undefined) {
+      alert("You have the todo already");
+      return;
+    }
+    if (isEdit === true) {
+      console.log(willUpdateTodo, " todo'yu güncelleyeceğiz");
+      const searchedTodo = todos.find((item) => item.id === willUpdateTodo);
+      const updatedTodo = {
+        ...searchedTodo,
+        text: todoText,
+      };
+      const filteredTodos = todos.filter((item) => item.id !== willUpdateTodo);
+      setTodos([...filteredTodos, updatedTodo]);
+      localStorage.setItem(
+        "todos",
+        JSON.stringify([...filteredTodos, updatedTodo])
+      );
+      setTodoText("");
+      setIsEdit(false);
+      setWillUpdateTodo("");
+    } else {
+      const newTodo = {
+        id: new Date().getTime(),
+        isDone: false,
+        text: todoText,
+        date: new Date(),
+      };
+      console.log("newTodo", newTodo);
+      setTodos([...todos, newTodo]);
+      localStorage.setItem("todos", JSON.stringify([...todos, newTodo]));
+      setTodoText("");
+    }
   };
   return (
     <div className="container">
       <h1 className="text-center my-5">Todo App</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="input-group mb-3">
-          <input
-            value={todoText}
-            type="text"
-            className="form-control"
-            placeholder="Type your todo"
-            onChange={(event) => setTodoText(event.target.value)}
-          />
-          <button className="btn btn-primary" type="submit">
-            ADD
-          </button>
-        </div>
-      </form>
+      <TodoForm
+        handleSubmit={handleSubmit}
+        todoText={todoText}
+        setTodoText={setTodoText}
+        isEdit={isEdit}
+      />
+      {todos.length <= 0 ? (
+        <p className="text-center my-5">You don't have any todos yet.</p>
+      ) : (
+        <>
+          {todos.map((item) => (
+            <Todo
+              item={item}
+              deleteTodo={deleteTodo}
+              setIsEdit={setIsEdit}
+              setWillUpdateTodo={setWillUpdateTodo}
+              setTodoText={setTodoText}
+              changeIsDone={changeIsDone}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 }
